3a7d07da42a7fc09abde1c36673fcee66c2cc8b4
rammohanpolu50 <mohanoraclefmw@gmail.com>
2018-10-17 11:40:42 -0400

    Update test.java

--- HIX/HIX/src/src/main/java/com/hix/base/test.java
+++ HIX/HIX/src/src/main/java/com/hix/base/test.java
@@ -3,7 +3,7 @@ package src.main.java.com.hix.base;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 
 }
